7dd89f1899cc7c8ad5edce59a0146ab6cd3b6e30
OlgaLesnykh <121940213+OlgaLesnykh@users.noreply.github.com>
2023-12-17 12:20:30 +0300

    Create variables.tf

---
+++ Diplom/Terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "yandex_cloud_token" {
+  type        = string
+  description = "Секретный токен от yandex_cloud"
+  default = "<Токен указать здесь>"
+}
+
+variable "yandex_cloud_id" {
+  type        = string
+  description = "ID облака"
+  default = "<ID облака указать здесь>"
+}
+
+variable "yandex_folder_id" {
+  type        = string
+  description = "ID каталога в облаке"
+  default = "ID каталога в облаке указать здесь"
+}
